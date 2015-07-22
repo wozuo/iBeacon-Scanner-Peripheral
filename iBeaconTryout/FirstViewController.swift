09885ccb4d65e1f55bc4192024e9f7db39ba7e04
gizmou <marctarnutzer@Marcs-MacBook-Pro.local>
2015-07-23 00:02:20 +0200

    Implemented Observer and Transmitter

--- iBeaconTryout/FirstViewController.swift
+++ iBeaconTryout/FirstViewController.swift
@@ -7,12 +7,50 @@
 //
 
 import UIKit
+import CoreLocation
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, CLLocationManagerDelegate, UITextViewDelegate {
+    @IBOutlet weak var consoleView: UITextView!
+    
+    var region = CLBeaconRegion()
+    var locationManager = CLLocationManager()
+    let uuid = NSUUID(UUIDString: "CF943B10-3D35-41F1-A9BA-2B92F9B49569")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self;
+        consoleView.delegate = self;
+        
+        //Request location services from user
+        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedAlways) {
+            locationManager.requestAlwaysAuthorization()
+        }
+        
+        region = CLBeaconRegion(proximityUUID: uuid, identifier: "Places")
+        locationManager.startMonitoringForRegion(region)
+        //locationManager.startRangingBeaconsInRegion(region)
+        consoleView.insertText(" <Console>: Start")
+    }
+    
+    func locationManager(manager: CLLocationManager!, didRangeBeacons beacons: [AnyObject]!, inRegion region: CLBeaconRegion!) {
+        println(beacons)
+        consoleView.insertText("\n <Console>: ")
+        for beacon in beacons {
+            //let client = beacon[0] as! NSString
+            println("im in ")
+            //consoleView.insertText(client as String)
+            
+            let currentBeacon = beacon as! CLBeacon
+            let minor = currentBeacon.minor.stringValue
+            let major = currentBeacon.major.stringValue
+            let consoleText = "-> Minor: \(minor) Major: \(major)"
+            consoleView.insertText(consoleText)
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, didEnterRegion region: CLRegion!) {
+        self.locationManager.startRangingBeaconsInRegion(region as! CLBeaconRegion)
     }
 
     override func didReceiveMemoryWarning() {
--- iBeaconTryout/SecondViewController.swift
+++ iBeaconTryout/SecondViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import CoreLocation
+import CoreBluetooth
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, CBPeripheralManagerDelegate, UITextViewDelegate {
+    @IBOutlet weak var consoleView: UITextView!
+    
+    let major: CLBeaconMajorValue = 1
+    let minor: CLBeaconMinorValue = 2
+    let uuid: NSUUID! = NSUUID(UUIDString: "CF943B10-3D35-41F1-A9BA-2B92F9B49569")
+    let identifier: String! = "Places"
+    var peripheralManager = CBPeripheralManager()
+    var advertisedData = NSDictionary()
+    var region = CLBeaconRegion()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        peripheralManager.delegate = self
+        consoleView.delegate = self
+
+        region = CLBeaconRegion(proximityUUID: uuid, major: major, minor: minor, identifier: identifier)
+        advertisedData = region.peripheralDataWithMeasuredPower(nil)
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil, options: nil)
+        consoleView.insertText(" <Console>: Start")
+    }
+    
+    func peripheralManagerDidUpdateState(peripheral: CBPeripheralManager!) {
+        switch peripheral.state {
+        case CBPeripheralManagerState.PoweredOn:
+            self.peripheralManager.startAdvertising(advertisedData as [NSObject : AnyObject])
+            consoleView.insertText("\n <Console>: Advertising... ")
+        default:
+            println("default")
+        }
     }
 
     override func didReceiveMemoryWarning() {
